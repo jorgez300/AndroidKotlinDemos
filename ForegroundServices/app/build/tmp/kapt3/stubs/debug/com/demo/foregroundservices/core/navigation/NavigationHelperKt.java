9474670ef3c4d75f3957dee03ccb2ac328c80d40
JorgeGonzalez <ing.jorge.gonzalez92@gmail.com>
2025-05-02 09:16:07 -0400

    vbsfv

---
+++ ForegroundServices/app/build/tmp/kapt3/stubs/debug/com/demo/foregroundservices/core/navigation/NavigationHelperKt.java
@@ -0,0 +1,16 @@
+package com.demo.foregroundservices.core.navigation;
+
+@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u0000\"\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0018\u0002\n\u0000\u001a\u0018\u0010\u0000\u001a\u00020\u00012\u0006\u0010\u0002\u001a\u00020\u00032\u0006\u0010\u0004\u001a\u00020\u0005H\u0007\u001a\u0016\u0010\u0006\u001a\u00020\u00012\u0006\u0010\u0007\u001a\u00020\b2\u0006\u0010\t\u001a\u00020\n\u00a8\u0006\u000b"}, d2 = {"NavigationHelper", "", "mainActivity", "Lcom/demo/foregroundservices/MainActivity;", "paddingValues", "Landroidx/compose/foundation/layout/PaddingValues;", "navigateTo", "screen", "", "navController", "Landroidx/navigation/NavHostController;", "app_debug"})
+public final class NavigationHelperKt {
+    
+    @androidx.compose.runtime.Composable()
+    public static final void NavigationHelper(@org.jetbrains.annotations.NotNull()
+    com.demo.foregroundservices.MainActivity mainActivity, @org.jetbrains.annotations.NotNull()
+    androidx.compose.foundation.layout.PaddingValues paddingValues) {
+    }
+    
+    public static final void navigateTo(@org.jetbrains.annotations.NotNull()
+    java.lang.Object screen, @org.jetbrains.annotations.NotNull()
+    androidx.navigation.NavHostController navController) {
+    }
+}
---
+++ ForegroundServices/app/build/tmp/kapt3/stubs/debug/com/demo/foregroundservices/screens/Home/HomeViewModel.java
@@ -0,0 +1,36 @@
+package com.demo.foregroundservices.screens.Home;
+
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000$\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0010\u000b\n\u0000\n\u0002\u0010\b\n\u0000\n\u0002\u0010\u000e\n\u0002\b\b\b\u0007\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002R\u0014\u0010\u0003\u001a\b\u0012\u0004\u0012\u00020\u00050\u0004X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u0014\u0010\u0006\u001a\b\u0012\u0004\u0012\u00020\u00070\u0004X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u0014\u0010\b\u001a\b\u0012\u0004\u0012\u00020\t0\u0004X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u0017\u0010\n\u001a\b\u0012\u0004\u0012\u00020\u00050\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000b\u0010\fR\u0017\u0010\r\u001a\b\u0012\u0004\u0012\u00020\u00070\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000e\u0010\fR\u0017\u0010\u000f\u001a\b\u0012\u0004\u0012\u00020\t0\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0010\u0010\f\u00a8\u0006\u0011"}, d2 = {"Lcom/demo/foregroundservices/screens/Home/HomeViewModel;", "Landroidx/lifecycle/ViewModel;", "()V", "_completed", "Landroidx/compose/runtime/MutableState;", "", "_id", "", "_title", "", "completed", "getCompleted", "()Landroidx/compose/runtime/MutableState;", "id", "getId", "title", "getTitle", "app_debug"})
+public final class HomeViewModel extends androidx.lifecycle.ViewModel {
+    @org.jetbrains.annotations.NotNull()
+    private final androidx.compose.runtime.MutableState<java.lang.Integer> _id = null;
+    @org.jetbrains.annotations.NotNull()
+    private final androidx.compose.runtime.MutableState<java.lang.Integer> id = null;
+    @org.jetbrains.annotations.NotNull()
+    private final androidx.compose.runtime.MutableState<java.lang.String> _title = null;
+    @org.jetbrains.annotations.NotNull()
+    private final androidx.compose.runtime.MutableState<java.lang.String> title = null;
+    @org.jetbrains.annotations.NotNull()
+    private final androidx.compose.runtime.MutableState<java.lang.Boolean> _completed = null;
+    @org.jetbrains.annotations.NotNull()
+    private final androidx.compose.runtime.MutableState<java.lang.Boolean> completed = null;
+    
+    public HomeViewModel() {
+        super();
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final androidx.compose.runtime.MutableState<java.lang.Integer> getId() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final androidx.compose.runtime.MutableState<java.lang.String> getTitle() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final androidx.compose.runtime.MutableState<java.lang.Boolean> getCompleted() {
+        return null;
+    }
+}
